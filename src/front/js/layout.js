11c3926679bcd4570f79b7f128999d35ef21347e
Fidel  Nieto <nietofidel25@gmail.com>
2024-10-21 20:13:30 -0400

    Add files via upload

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,27 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Users from "./Users";
-import UsersDetails from "./UsersDetails";
-import { AppProvider } from "./store/AppProvider";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./Home";
+import Private from "./Private";
+import Signup from "./Signup";
+import Login from "./Login";
+import Logout from "./Logout";
 
 const Layout = () => {
   const basename = process.env.BASENAME || "";
 
-  if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "")
-    return <BackendURL />;
-
   return (
     <div>
-      <h1>Todolist + Users App</h1>
-      <AppProvider>
-        <BrowserRouter basename={basename}>
-          <Routes>
-            <Route element={<Users />} path="/" />
-            <Route element={<UsersDetails />} path="/users/:userId" />
-            <Route element={<h1>Not found!</h1>} />
-          </Routes>
-        </BrowserRouter>
-      </AppProvider>
+      <BrowserRouter basename={basename}>
+        <Logout />
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Signup />} path="/signup" />
+          <Route element={<Private />} path="/private" />
+          <Route element={<h1>Not found!</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
