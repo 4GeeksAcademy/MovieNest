dbf83d3c060b5e7f79e022068e04bfd52d363c65
Fidel  Nieto <nietofidel25@gmail.com>
2024-10-25 22:30:32 +0000

    finishing database models, adding only Users and Favorites

--- src/api/models.py
+++ src/api/models.py
@@ -4,15 +4,36 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)  # Campo username
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    is_active = db.Column(db.Boolean(), default=True, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.username}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
+    movie_id = db.Column(db.String(120), nullable=False)
+    movie_name = db.Column(db.String(120), nullable=False)
+
+    user = db.relationship('User', backref='favorites', lazy=True)  
+
+    def __repr__(self):
+        return f'<Favorite {self.movie_name} for user {self.user_id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "movie_id": self.movie_id,
+            "movie_name": self.movie_name
         }
